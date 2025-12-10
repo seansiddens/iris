6e5389cd195392aa4ba4363f6f71c76bc3dc29e5
Sean Siddens <seansiddens@gmail.com>
2025-12-10 14:21:40 -0600

    WG specialization and proper atomics

--- examples/spatial_fusion_microbenchmark/spatial_fusion.py
+++ examples/spatial_fusion_microbenchmark/spatial_fusion.py
@@ -27,19 +27,21 @@ def producer_kernel(
     for tile_id in range(num_tiles):
         block_start = tile_id * BLOCK_SIZE
         offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        offsets = tl.multiple_of(offsets, BLOCK_SIZE)
         mask = offsets < NUM_ELEMENTS 
 
         x = tl.load(buffer + offsets, mask=mask)
 
         # tl.store(buffer + offsets, x+x, mask=mask, cache_modifier=".wt")
-        # tl.store(buffer + offsets, x, mask=mask, cache_modifier=".cg")
-        tl.store(buffer + offsets, x+x, mask=mask)
+        tl.store(buffer + offsets, x, mask=mask, cache_modifier=".cg")
+        # tl.store(buffer + offsets, x+x, mask=mask)
 
         tl.debug_barrier()
 
-        # tl.store(lock, 1, cache_modifier=".wt")
-        # tl.store(lock, 1, cache_modifier=".cg")
-        tl.store(lock + tile_id, 1)
+        # tl.store(lock + tile_id, 1, cache_modifier=".wt")
+        # tl.store(lock + tile_id, 1, cache_modifier=".cg")
+        # tl.store(lock + tile_id, 1)
+        tl.atomic_xchg(lock + tile_id, 1)
 
 @triton.jit()
 def consumer_kernel(
@@ -63,13 +65,73 @@ def consumer_kernel(
     num_tiles = tl.cdiv(NUM_ELEMENTS, BLOCK_SIZE)
 
     for tile_id in range(num_tiles):
-        while tl.load(lock + tile_id, cache_modifier=".cv", volatile=True) != 1:
+        # while tl.load(lock + tile_id, cache_modifier=".cv", volatile=True) != 1:
+        #     pass
+
+        while tl.atomic_cas(lock + tile_id, 1, 1) != 1:
             pass
 
         # Lock acquired, read tile.
         block_start = tile_id * BLOCK_SIZE
         offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        offsets = tl.multiple_of(offsets, BLOCK_SIZE)
         mask = offsets < NUM_ELEMENTS 
 
         x = tl.load(buffer + offsets, mask=mask)
         tl.store(output_buffer + offsets, x, mask=mask)
+
+
+@triton.jit()
+def workgroup_specialized_kernel(
+    buffer,
+    output_buffer,
+    lock,
+    producer_xcd,
+    consumer_xcd,
+    NUM_ELEMENTS: tl.constexpr,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    xcd_id = read_xcd_id()
+
+    # Early exit for all pids except 0 and 8
+    if pid != 0 and pid != 8:
+        return
+
+    # pid 0 executes producer logic
+    if pid == 0:
+        tl.store(producer_xcd, xcd_id)
+
+        num_tiles = tl.cdiv(NUM_ELEMENTS, BLOCK_SIZE)
+        for tile_id in range(num_tiles):
+            block_start = tile_id * BLOCK_SIZE
+            offsets = block_start + tl.arange(0, BLOCK_SIZE)
+            offsets = tl.multiple_of(offsets, BLOCK_SIZE)
+            mask = offsets < NUM_ELEMENTS 
+
+            x = tl.load(buffer + offsets, mask=mask)
+
+            tl.store(buffer + offsets, x, mask=mask, cache_modifier=".cg")
+
+            tl.debug_barrier()
+
+            tl.atomic_xchg(lock + tile_id, 1)
+
+    # pid 8 executes consumer logic
+    elif pid == 8:
+        tl.store(consumer_xcd, xcd_id)
+
+        num_tiles = tl.cdiv(NUM_ELEMENTS, BLOCK_SIZE)
+
+        for tile_id in range(num_tiles):
+            while tl.atomic_cas(lock + tile_id, 1, 1) != 1:
+                pass
+
+            # Lock acquired, read tile.
+            block_start = tile_id * BLOCK_SIZE
+            offsets = block_start + tl.arange(0, BLOCK_SIZE)
+            offsets = tl.multiple_of(offsets, BLOCK_SIZE)
+            mask = offsets < NUM_ELEMENTS 
+
+            x = tl.load(buffer + offsets, mask=mask)
+            tl.store(output_buffer + offsets, x, mask=mask)
